53035f353ec7d3a9958a84d4334497ecd8856e03
Denis GERMAIN <dt.germain@gmail.com>
2023-07-11 22:09:19 +0200

    chore: add theme

--- main.go
+++ main.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"gocastle/screens"
+	"image/color"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/theme"
 )
 
+type CustomTheme struct{}
+
+func (t CustomTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
+	return theme.DefaultTheme().Color(name, variant)
+}
+
+func (t CustomTheme) Icon(name fyne.ThemeIconName) fyne.Resource {
+	return theme.DefaultTheme().Icon(name)
+}
+
+func (t CustomTheme) Font(style fyne.TextStyle) fyne.Resource {
+	return theme.DefaultTheme().Font(style)
+}
+
+func (t CustomTheme) Size(s fyne.ThemeSizeName) float32 {
+	return 15
+}
+
 func main() {
-	myApp := app.New()
-	mainWindow := myApp.NewWindow("GoCastle")
+	goCastle := app.New()
+	goCastle.Settings().SetTheme(&CustomTheme{})
+	mainWindow := goCastle.NewWindow("GoCastle")
 
 	screens.ShowMenuScreen(mainWindow)
 
