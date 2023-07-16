fe268de8cd30d402c3d66ffa8254d8ac7fb483e6
Denis GERMAIN <dt.germain@gmail.com>
2023-07-16 10:25:02 +0200

    chore: clean a bit scrollable map

--- screens/map.go
+++ screens/map.go
@@ -8,42 +8,41 @@ import (
 
 func ShowMapScreen(window fyne.Window) {
 	h, v := 30, 30
-	firstLine := container.NewGridWithColumns(h)
-	vBorder := container.NewGridWithRows(v - 1)
-
 	imageMatrix := createMapMatrix(h, v)
+	firstLine := container.NewHBox()
+
 	player := canvas.NewImageFromFile("./static/warrior.png")
 	player.FillMode = canvas.ImageFillOriginal
 	player.Resize(fyne.NewSize(32, 32))
 
-	usableMapContainer := container.NewWithoutLayout()
+	horizontalBorder := canvas.NewImageFromFile("static/black_hline.png")
+	horizontalBorder.FillMode = canvas.ImageFillOriginal
+	horizontalBorder.Resize(fyne.NewSize(float32(h-1)*32, 1))
+	firstLine.Add(horizontalBorder)
+	verticalBorder := canvas.NewImageFromFile("static/black_vline.png")
+	verticalBorder.FillMode = canvas.ImageFillOriginal
+	verticalBorder.Resize(fyne.NewSize(1, float32(v-1)*32))
+	secondLine := container.NewHBox(verticalBorder)
 
+	mapContainer := container.NewWithoutLayout()
 	for i := 0; i < v; i++ {
+		currentLine := float32(i) * 32
 		for j := 0; j < h; j++ {
 			tile := imageMatrix[i][j]
 			tile.Resize(fyne.NewSize(32, 32))
-			if i == 0 {
-				firstLine.Add(tile)
-			} else {
-				if j == 0 {
-					vBorder.Add(tile)
-				} else {
-					currentPos := fyne.NewPos(float32(i-1)*32, float32(j-1)*32)
-					tile.Move(currentPos)
-					usableMapContainer.Add(tile)
-					if i == 1 && j == 1 {
-						player.Move(currentPos)
-						usableMapContainer.Add(player)
-					}
-				}
+			currentPos := fyne.NewPos(currentLine, float32(j)*32)
+			tile.Move(currentPos)
+			mapContainer.Add(tile)
+			if i == 2 && j == 4 {
+				player.Move(currentPos)
+				mapContainer.Add(player)
 			}
 		}
 	}
+	secondLine.Add(mapContainer)
 
-	secondLine := container.NewHBox(vBorder, usableMapContainer)
-	mapContainer := container.NewVBox(firstLine, secondLine)
-
-	content := container.NewScroll(mapContainer)
+	scrollableMapContainer := container.NewVBox(firstLine, secondLine)
+	content := container.NewScroll(scrollableMapContainer)
 
 	window.SetContent(content)
 }
