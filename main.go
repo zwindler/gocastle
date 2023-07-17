e364aa7f1a5f009d662e02cd4b9fee24b4c5d666
Denis GERMAIN <dt.germain@gmail.com>
2023-07-17 21:31:30 +0200

    chore: move theme to model package

--- main.go
+++ main.go
@@ -1,44 +1,16 @@
 package main
 
 import (
+	"gocastle/model"
 	"gocastle/screens"
-	"image/color"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/theme"
 )
-
-type CustomTheme struct{}
-
-func (t CustomTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
-	return theme.DefaultTheme().Color(name, variant)
-}
-
-func (t CustomTheme) Icon(name fyne.ThemeIconName) fyne.Resource {
-	return theme.DefaultTheme().Icon(name)
-}
-
-func (t CustomTheme) Font(style fyne.TextStyle) fyne.Resource {
-	return theme.DefaultTheme().Font(style)
-}
-
-func (t CustomTheme) Size(style fyne.ThemeSizeName) float32 {
-	switch style {
-	case theme.SizeNameInnerPadding:
-		return 14
-	case theme.SizeNamePadding:
-		return 0
-	case theme.SizeNameText:
-		return 16
-	default:
-		return theme.DefaultTheme().Size(style)
-	}
-}
 
 func main() {
 	goCastle := app.New()
-	goCastle.Settings().SetTheme(&CustomTheme{})
+	goCastle.Settings().SetTheme(&model.CustomTheme{})
 	mainWindow := goCastle.NewWindow("GoCastle")
 
 	screens.ShowMenuScreen(mainWindow)
