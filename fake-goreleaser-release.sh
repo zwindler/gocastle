3cf2b3b152eed71a28e5a674b9089537d8f47150
Denis GERMAIN <dt.germain@gmail.com>
2023-07-31 22:26:26 +0200

    chore: improve fake-goreleaser

--- fake-goreleaser-release.sh
+++ fake-goreleaser-release.sh
@@ -3,19 +3,33 @@
 GOBIN=~/go/bin/
 PATH=$PATH:$GOBIN
 
-mkdir -p dist/gocastle_linux_amd64_v1/ dist/gocastle_android_amd64 dist/gocastle_android_arm64/ dist/gocastle_windows_amd64_v1/
+mkdir -p build/
 
+# android
 fyne-cross android -arch=arm64 -app-id fr.zwindler.gocastle
-cp fyne-cross/dist/android-arm64/gocastle.apk dist/gocastle_android_amd64/
+cp fyne-cross/dist/android-arm64/gocastle.apk build/
 
+# linux
 fyne-cross linux -arch=amd64,arm64 -app-id fr.zwindler.gocastle
-cp fyne-cross/dist/linux-amd64/gocastle.tar.xz dist/gocastle_linux_amd64_v1/
-cp fyne-cross/dist/linux-arm64/gocastle.tar.xz dist/gocastle_linux_arm64_v1/
+cp fyne-cross/dist/linux-amd64/gocastle.tar.xz build/gocastle.amd64.tar.xz
+cp fyne-cross/dist/linux-arm64/gocastle.tar.xz build/gocastle.arm64.tar.xz
 
+# windows
 fyne-cross windows -app-id fr.zwindler.gocastle
-cp fyne-cross/dist/windows-amd64/gocastle.exe.zip dist/gocastle_windows_amd64_v1/gocastle.exe.zip
-cd dist/gocastle_windows_amd64_v1/
-unzip gocastle.exe.zip
-cd ../..
+cp fyne-cross/dist/windows-amd64/gocastle.exe.zip build/gocastle_windows_amd64_v1/gocastle.exe.zip
 
+# macos TODO
+# fyne-cross darwin -arch=amd64,arm64 --macosx-sdk-path /home/zwindler/sources/gocastle/bin/SDKs -app-id fr.zwindler.gocastle
+
+# clean fyn-cross dir
 rm -rf fyne-cross/
+
+# unzip archives
+cd build/
+unzip gocastle.exe.zip && rm gocastle.exe.zip
+tar xJf gocastle.amd64.tar.xz
+mv usr/local/bin/gocastle ./gocastle.amd64
+rm gocastle.amd64.tar.xz
+tar xJf gocastle.arm64.tar.xz
+mv usr/local/bin/gocastle ./gocastle.arm64
+rm gocastle.arm64.tar.xz
